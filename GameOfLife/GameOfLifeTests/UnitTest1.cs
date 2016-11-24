612bdba235906b509db3af3777982e86a7a31ef8
hatelove <jay.hatelove@gmail.com>
2016-11-25 01:05:14 +0800

    fulfill active death scenario

--- GameOfLife/GameOfLifeTests/UnitTest1.cs
+++ GameOfLife/GameOfLifeTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using ExpectedObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -27,14 +28,15 @@ namespace GameOfLifeTests
         }
 
         [TestMethod]
-        public void three_near_cells_one_alive()
+        public void three_near_cells_3_alive()
         {
             var cells = new List<Cell> { new Cell(0, 0), new Cell(1, 0), new Cell(2, 0) };
             var world = new World(cells);
             List<Cell> liveCells = world.NextMoment();
-            Assert.AreEqual(1, liveCells.Count);
-            Assert.AreEqual(1, liveCells[0].X);
-            Assert.AreEqual(0, liveCells[0].Y);
+            Assert.AreEqual(3, liveCells.Count);
+
+            var expected = new List<Cell> { new Cell(1, 0), new Cell(1, -1), new Cell(1, 1) };
+            expected.ToExpectedObject().ShouldEqual(liveCells);
         }
 
         [TestMethod]
@@ -73,7 +75,7 @@ namespace GameOfLifeTests
         {
             var cells = new List<Cell> { new Cell(-1, 0), new Cell(-1, 1), new Cell(0, 0), new Cell(0, 1), new Cell(1, 1) };
             var world = new World(cells);
-            List<Cell> liveCells = world.NextMoment();            
+            List<Cell> liveCells = world.NextMoment();
             var expected = new List<Cell> { new Cell(-1, 0), new Cell(-1, 1), new Cell(1, 1) };
             expected.ToExpectedObject().ShouldEqual(liveCells);
         }
@@ -88,25 +90,98 @@ namespace GameOfLifeTests
             this.cells = cells;
         }
 
+        private Dictionary<bool, IPointChanger> changers = new Dictionary<bool, IPointChanger>
+        {
+            {true, new LivePointChanger() },
+            {false, new DeathPointChanger() },
+        };
+
         internal List<Cell> NextMoment()
         {
-            var result = new List<Cell>();
-            foreach (var cell in this.cells)
+            //以最小x,y與最大x,y兩點長出二維table, 巡覽每一個點
+
+            var pointsOfTable = new HashSet<Point>();
+            cells.ForEach(cell => pointsOfTable.Add(new Point(cell.X, cell.Y) { IsLive = true }));
+
+            var minX = this.cells.Min(cell => cell.X) - 1;
+            var minY = this.cells.Min(cell => cell.Y) - 1;
+
+            var maxX = this.cells.Max(cell => cell.X) + 1;
+            var maxY = this.cells.Max(cell => cell.Y) + 1;
+            var points = new List<Point>();
+            for (int x = minX; x <= maxX; x++)
             {
-                //var table = GetTable(Tuple.Create(cell.X, cell.Y));
-                var x_start = cell.X - 1;
-                var y_start = cell.Y - 1;
-                var x_end = cell.X + 1;
-                var y_end = cell.Y + 1;
-                var matchCount = this.cells.Count(c => c.X >= x_start && c.X <= x_end && c.Y >= y_start && c.Y <= y_end);
-                if (matchCount >= 3 && matchCount < 5)
+                for (int y = minY; y <= maxY; y++)
                 {
-                    result.Add(new Cell(cell.X, cell.Y));
+                    pointsOfTable.Add(new Point(x, y) { IsLive = false });
                 }
+            }
+
+            var result = new List<Cell>();
+            foreach (var point in pointsOfTable)
+            {
+                var changer = changers[point.IsLive];
+                changer.Handle(point, cells.Select(x => new Point(x.X, x.Y)).ToList(), () => result.Add(new Cell(point.x, point.y)));
             }
 
             return result;
         }
+    }
+
+    internal class DeathPointChanger : IPointChanger
+    {
+        public void Handle(Point point, List<Point> list, Action handler)
+        {
+            var x_start = point.x - 1;
+            var y_start = point.y - 1;
+            var x_end = point.x + 1;
+            var y_end = point.y + 1;
+            var matchCount = list.Count(c => c.x >= x_start && c.x <= x_end && c.y >= y_start && c.y <= y_end);
+            if (matchCount == 3) { handler.Invoke(); }
+        }
+    }
+
+    internal class LivePointChanger : IPointChanger
+    {
+        public void Handle(Point point, List<Point> list, Action handler)
+        {
+            var x_start = point.x - 1;
+            var y_start = point.y - 1;
+            var x_end = point.x + 1;
+            var y_end = point.y + 1;
+            var matchCount = list.Count(c => c.x >= x_start && c.x <= x_end && c.y >= y_start && c.y <= y_end);
+            if (matchCount >= 3 && matchCount < 5) { handler.Invoke(); }
+        }
+    }
+
+    internal interface IPointChanger
+    {
+        void Handle(Point point, List<Point> list, Action handler);
+    }
+
+    internal class Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var point = (Point)obj;
+            return this.x == point.x && this.y == point.y;
+        }
+
+        public override int GetHashCode()
+        {
+            return Tuple.Create(this.x, this.y).GetHashCode();
+        }
+
+        public bool IsLive { get; internal set; }
     }
 
     internal class Cell
