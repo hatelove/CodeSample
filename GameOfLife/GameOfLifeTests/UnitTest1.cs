b51ad3692f513f8d564e931a4f655dc4a61b5f8e
hatelove <jay.hatelove@gmail.com>
2016-11-24 19:48:51 +0800

    without any cell

--- GameOfLife/GameOfLifeTests/UnitTest1.cs
+++ GameOfLife/GameOfLifeTests/UnitTest1.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 
 namespace GameOfLifeTests
 {
@@ -7,8 +8,37 @@ namespace GameOfLifeTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void without_any_cell()
         {
+            var cells = new List<Cell>();
+            var world = new World(cells);
+
+            List<Cell> result = world.NextMoment();
+
+            Assert.AreEqual(0, result.Count);
+        }
+    }
+
+    internal class Cell
+    {
+    }
+
+    internal class World
+    {
+        private List<Cell> cells;
+
+        public World()
+        {
+        }
+
+        public World(List<Cell> cells)
+        {
+            this.cells = cells;
+        }
+
+        internal List<Cell> NextMoment()
+        {
+            return new List<Cell>();
         }
     }
 }
