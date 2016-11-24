2a569e4cef3aa27aa833e4c679f9182dee2c42d0
hatelove <jay.hatelove@gmail.com>
2016-11-24 19:57:04 +0800

    only one cell

--- GameOfLife/GameOfLifeTests/UnitTest1.cs
+++ GameOfLife/GameOfLifeTests/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GameOfLifeTests
 {
@@ -17,10 +17,48 @@ namespace GameOfLifeTests
 
             Assert.AreEqual(0, result.Count);
         }
+
+        [TestMethod]
+        public void only_one_cell()
+        {
+            var cells = new List<Cell>
+            {
+                new Cell(0,0,Status.Live),
+            };
+
+            var world = new World(cells);
+
+            var result = world.NextMoment();
+
+            Assert.AreEqual(Status.Death, result.First().Status);
+        }
+    }
+
+    internal enum Status
+    {
+        None,
+        Live,
+        Death
     }
 
     internal class Cell
     {
+        private Status status;
+        private int _x;
+        private int _y;
+
+        public Cell(int x, int y, Status status)
+        {
+            this._x = x;
+            this._y = y;
+            this.status = status;
+        }
+
+        public Status Status
+        {
+            get { return this.status; }
+            set { this.status = value; }
+        }
     }
 
     internal class World
@@ -38,7 +76,17 @@ namespace GameOfLifeTests
 
         internal List<Cell> NextMoment()
         {
-            return new List<Cell>();
+            if (!this.cells.Any())
+            {
+                return this.cells;
+            }
+
+            if (this.cells.First().Status == Status.Live)
+            {
+                this.cells.First().Status = Status.Death;
+            }
+
+            return this.cells;
         }
     }
 }
