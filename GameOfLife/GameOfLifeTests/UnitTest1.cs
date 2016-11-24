fd1152e7bffc940d30132b0808daadfa99cc9934
hatelove <jay.hatelove@gmail.com>
2016-11-24 22:34:56 +0800

    restart with new session

--- GameOfLife/GameOfLifeTests/UnitTest1.cs
+++ GameOfLife/GameOfLifeTests/UnitTest1.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Linq;
 using System;
 
 namespace GameOfLifeTests
@@ -9,46 +8,12 @@ namespace GameOfLifeTests
     public class UnitTest1
     {
         [TestMethod]
-        public void one_cell()
+        public void one_cell_alive_to_death()
         {
             var cells = new List<Cell> { new Cell(0, 0) };
-
             var world = new World(cells);
-
             List<Cell> liveCells = world.NextMoment();
-
             Assert.AreEqual(0, liveCells.Count);
-        }
-
-        [TestMethod]
-        public void three_cells_near()
-        {
-            var cells = new List<Cell> { new Cell(-1, 0), new Cell(0, 0), new Cell(1, 0) };
-            var world = new World(cells);
-            var liveCells = world.NextMoment();
-            Assert.AreEqual(1, liveCells.Count);
-            Assert.AreEqual(0, liveCells[0].X);
-            Assert.AreEqual(0, liveCells[0].Y);
-        }
-
-        [TestMethod]
-        public void three_cells_not_near()
-        {
-            var cells = new List<Cell> { new Cell(-2, 0), new Cell(0, 0), new Cell(1, 0) };
-            var world = new World(cells);
-            var liveCells = world.NextMoment();
-            Assert.AreEqual(0, liveCells.Count);
-        }
-
-        [TestMethod]
-        public void four_cells_not_near()
-        {
-            var cells = new List<Cell> { new Cell(-1, 1), new Cell(1, 1), new Cell(0, 0), new Cell(-1, -1) };
-            var world = new World(cells);
-            var liveCells = world.NextMoment();
-            Assert.AreEqual(1, liveCells.Count);
-            Assert.AreEqual(0, liveCells[0].X);
-            Assert.AreEqual(0, liveCells[0].Y);
         }
     }
 
@@ -63,46 +28,19 @@ namespace GameOfLifeTests
 
         internal List<Cell> NextMoment()
         {
-            foreach (var cell in this.cells)
-            {
-                cell.IsAlive = CheckCellShouldLive(cell, this.cells);
-            }
-            return this.cells.Where(x => x.IsAlive).ToList();
-        }
-
-        private bool CheckCellShouldLive(Cell cell, List<Cell> cells)
-        {
-            var count = cells.Count(item => isNear(cell, item));
-            return count >= 3;
-        }
-
-        private static bool isNear(Cell cell, Cell another)
-        {
-            var isNotNear = another.X > (cell.X + 1) || another.Y > (cell.Y + 1);
-            var isNotNear_2 = another.X < (cell.X - 1) || another.Y < (cell.Y - 1);
-            return !(isNotNear || isNotNear_2);
+            throw new NotImplementedException();
         }
     }
 
     internal class Cell
     {
-        private int x;
-        private int y;
-        private bool _isAlive = true;
+        private int _x;
+        private int _y;
 
         public Cell(int x, int y)
         {
-            this.x = x;
-            this.y = y;
-        }
-
-        public bool IsAlive
-        {
-            get { return this._isAlive; }
-            set { this._isAlive = value; }
-        }
-
-        public int X { get { return this.x; } }
-        public int Y { get { return this.y; } }
+            this._x = x;
+            this._y = y;
+        }              
     }
 }
